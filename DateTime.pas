74a4aa5219461561d434fd4d22ac93d8cda826c4
имя фамилия <anuchin@sfedu.ru>
2021-10-21 10:31:32 +0300

    Revert "Add function SecondsInMinute"
    This reverts commit 3f3247a2419718111551ab3ae65a05dd31d35dd1.

--- DateTime.pas
+++ DateTime.pas
@@ -2,13 +2,12 @@
 function IsLeapYear(Year : Integer): Boolean;
 Begin
   var b := False;
-  if (Year mod 4 = 0) and (Year mod 100 <> 0) then
-    b := True
-  else if (Year mod 4 = 0) and (Year mod 400 = 0) then
+  if (Year % 4 = 0) and (Year % 100 != 0) then
+    b := True;
+  else if (Year % 4 = 0) and (Year % 400 = 0) then
     b := True;
   result := b;
 end;
-function SecondsInMinute():= 60;
 begin
   
 end.
