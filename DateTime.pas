c4242998baf990c2f9de8517bda70845bf0a0138
имя фамилия <anuchin@sfedu.ru>
2021-10-21 10:29:10 +0300

    Add function ISLeapYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,13 @@
 ﻿uses System;
-
+function IsLeapYear(Year : Integer): Boolean;
+Begin
+  var b := False;
+  if (Year % 4 = 0) and (Year % 100 != 0) then
+    b := True;
+  else if (Year % 4 = 0) and (Year % 400 = 0) then
+    b := True;
+  result := b;
+end;
 begin
   
 end.
